1acd184d59c19d15489cbf475f31be9204c121ce
Danielanunez <naninunez0@gmail.com>
2022-12-14 21:48:20 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const homeRoutes = require('./src/routes/homeRoutes');
+const registerRoutes = require('./src/routes/registerRoutes');
+
+
+/*Archivos estaticos */
+app.use('/', express.static('public'));
+
+/* Archivos dinámicos con EJS */
+app.use('/', homeRoutes);
+app.use('/register', registerRoutes);
+
+/*Configaracion del motor de plantillas js */
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './src/views'));
+
+app.use((req,res) => {
+    res.status(404).send('Lo siento página no encontrada');
+});
+
+/* Puerto*/
+app.listen(PORT, () => console.log( `Servidor corriendo desde http://localhost:${PORT}`));
+
